22ae2f90024b61e6acff1e62ca99d6a073302868
Alkesh Desai <alkeshdesai001@gmail.com>
2020-03-21 23:21:06 +0530

    Add Copy Overlay and Message

--- src/components/palette/colorBox/ColorBox.js
+++ src/components/palette/colorBox/ColorBox.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import './ColorBox.css';
 
-const ColorBox = ({ background, name }) => (
-  <CopyToClipboard text={background}>
-    <div style={{ background }} className='ColorBox'>
-      <div className='CopyContainer'>
-        <div className='BoxContent'>
-          <span>{name}</span>
+const ColorBox = ({ background, name }) => {
+  const [copied, setCopied] = useState(false);
+
+  const setCopyHandler = () => setCopied(true);
+
+  useEffect(() => {
+    setTimeout(() => setCopied(false), 1500);
+  }, [copied]);
+
+  return (
+    <CopyToClipboard text={background} onCopy={() => setCopyHandler()}>
+      <div style={{ background }} className='ColorBox'>
+        <div
+          style={{ background }}
+          className={`CopyOverlay ${copied && 'Show'}`}
+        />
+        <div style={{ background }} className={`CopyMsg ${copied && 'Show'}`}>
+          <h1>Copied</h1>
+          <p>{background}</p>
         </div>
-        <button className='CopyButton'>Copy</button>
+        <div className='CopyContainer'>
+          <div className='BoxContent'>
+            <span>{name}</span>
+          </div>
+          <button className='CopyButton'>Copy</button>
+        </div>
+        <span className='SeeMore'>More</span>
       </div>
-      <span className='SeeMore'>MORE</span>
-    </div>
-  </CopyToClipboard>
-);
+    </CopyToClipboard>
+  );
+};
 
 export default ColorBox;
